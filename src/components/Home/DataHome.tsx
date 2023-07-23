f543a504c78e7ddc26f560a851d2884263605da0
vickyadri29 <vickyadri290501@gmail.com>
2023-07-24 03:06:40 +0800

    fsdfds

---
+++ src/components/Home/DataHome.tsx
@@ -0,0 +1,12 @@
+import ig from '/icons/ig.svg'
+
+const DataHome: React.FC = () => {
+    console.log('test!')
+  return (
+    <div>
+      <img src={ig} alt="fdsfds fdsfsd" />
+    </div>
+  );
+};
+
+export default DataHome;
